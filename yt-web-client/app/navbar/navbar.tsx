f9ea94f6ed78ecd7db9cc027bc88b1547ddeb2b0
Michael S <101059605+mls26155@users.noreply.github.com>
2023-09-12 18:12:14 -0400

    Add files via upload

---
+++ yt-web-client/app/navbar/navbar.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import SignIn from "./sign-in";
+import Link from "next/link";
+
+import styles from "./navbar.module.css";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { onAuthStateChangedHelper } from "../firebase/firebase";
+import { User } from "firebase/auth";
+import Upload from "./upload";
+
+
+function NavBar() {
+  // Initialize user state
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedHelper((user) => {
+      setUser(user);
+    });
+
+    // Cleanup subscription on unmount
+    return () => unsubscribe();
+  }, [] /* No dependencies, never rerun */);
+
+
+  return (
+    <nav className={styles.nav}>
+      <Link href="/">
+        <Image width={270} height={60}
+          src="/youtube-logo.svg" alt="YouTube Logo"/>
+      </Link>
+      { 
+        user && <Upload />
+      }
+      <SignIn user={user} />
+    </nav>
+  );  
+}
+
+export default NavBar;
